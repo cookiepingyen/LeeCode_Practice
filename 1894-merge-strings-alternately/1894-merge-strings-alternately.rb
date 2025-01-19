674dc09813fe8b16a2aec11d6de741800370dce2
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-01-19 11:19:01 +0800

    Time: 60 ms (58.94%), Space: 211.9 MB (19.55%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.rb
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.rb
@@ -2,31 +2,20 @@
 # @param {String} word2
 # @return {String}
 def merge_alternately(word1, word2)
-  word1_max_index = word1.length - 1
-  word2_max_index = word2.length - 1
+  len = (word1.length > word2.length)? word1.length : word2.length
+  cur_index = 0
+  str = ''
 
-  str = ""
-  index_1 = 0
-  index_2 = 0
-
-  while(index_1 <= word1_max_index && index_2 <= word2_max_index)
-    if(index_1 <= index_2)
-      str += word1[index_1]
-      index_1 += 1
-    else
-      str += word2[index_2]
-      index_2 += 1
+  while(cur_index <= len - 1)
+    if(word1[cur_index])
+      str += word1[cur_index]
     end
-  end
 
-  while(index_1 <= word1_max_index)
-    str += word1[index_1]
-    index_1 += 1
-  end
+    if(word2[cur_index])
+      str += word2[cur_index]
+    end
 
-  while(index_2 <= word2_max_index)
-    str += word2[index_2]
-    index_2 += 1
+    cur_index += 1
   end
 
   str
