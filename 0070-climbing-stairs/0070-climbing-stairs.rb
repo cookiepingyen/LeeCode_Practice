11fe73b4a1010f57da0dd783dfd8ddea2b7b3145
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-01-23 17:13:54 +0800

    Time: 0 ms (100%), Space: 211.4 MB (18.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.rb
@@ -0,0 +1,15 @@
+# @param {Integer} n
+# @return {Integer}
+def climb_stairs(n)
+  return n if(n == 1 || n == 2)
+  dp = [];
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for i in 3..n
+    dp[i] = dp[i-1] + dp[i-2]
+    puts "i:#{i} dp[#{i}]:#{dp[i]}"
+  end
+
+  dp[n]
+end
