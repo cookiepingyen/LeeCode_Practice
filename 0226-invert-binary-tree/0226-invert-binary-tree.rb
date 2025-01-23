528089924b427e2041e57562889dca9ccd01d65a
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-01-23 23:19:55 +0800

    Time: 0 ms (100%), Space: 211.2 MB (100%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.rb
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+  return nil if root == nil
+  root.left, root.right = root.right, root.left
+
+  invert_tree(root.left) if root.left != nil
+  invert_tree(root.right) if root.right != nil
+
+  return root;
+end
