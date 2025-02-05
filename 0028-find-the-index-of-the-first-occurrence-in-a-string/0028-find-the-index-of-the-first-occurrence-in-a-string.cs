268f11b0ac05e554b5cefba03be0d28b620ff137
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-02-05 16:58:33 +0800

    Time: 0 ms (100%), Space: 39.5 MB (35.89%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -1,5 +1,10 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-      return haystack.IndexOf(needle);
+      for(int i = 0; i <= haystack.Length - needle.Length; i++){
+        if(haystack.Substring(i, needle.Length) == needle){
+          return i;
+        }
+      }
+      return -1;
     }
 }
