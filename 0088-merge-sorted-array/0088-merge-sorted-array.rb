7d53f0854368c94cdc17d9c69ab83dca16ecb8ae
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-04-05 16:04:10 +0800

    Time: 0 ms (100%), Space: 211.7 MB (11.96%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.rb
+++ 0088-merge-sorted-array/0088-merge-sorted-array.rb
@@ -4,21 +4,25 @@
 # @param {Integer} n
 # @return {Void} Do not return anything, modify nums1 in-place instead.
 def merge(nums1, m, nums2, n)
-    return if n == 0
-    max_index = nums1.length - 1
-    while(n > 0 && m > 0)
-        if nums2[n-1] >= nums1[m-1]
-             nums1[max_index] = nums2[n-1]
-             n-=1
-        else
-             nums1[max_index] = nums1[m-1]
-              m-=1
-        end
-        max_index-=1
+  return if nums2.length == 0
+
+  # 從最後面開始把最大的加進去，避免從頭開始蓋掉原本的 num1
+  max = nums1.length - 1
+
+  while(m > 0 && n > 0)
+    if(nums1[m-1] < nums2[n-1])
+      nums1[max] = nums2[n-1]
+      n -= 1
+    else
+      nums1[max] = nums1[m-1]
+      m -= 1
     end
-    while(n > 0)
-        nums1[max_index] = nums2[n-1]
-        n -= 1
-        max_index -= 1
-    end   
+    max -= 1
+  end
+
+  while(n > 0)
+    nums1[max] = nums2[n-1]
+    n -= 1
+    max -= 1
+  end
 end
