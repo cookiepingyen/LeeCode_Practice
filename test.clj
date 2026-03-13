18e8b5bb4fee0082b4fb460575324c4b0824227f
Yen <yen.wu@abagile.com>
2026-03-13 17:38:54 +0800

    update test.clj

--- test.clj
+++ test.clj
@@ -1,14 +1,10 @@
 ; run clojure -M test.clj
 
-(require '[clojure.string :as str])
-
-(defn high-and-low [s]
-  ; 把字串轉成 str_array，再轉成 int_array
-  (let [nums (map #(Integer/parseInt %) (str/split s #" "))
-        max-num (apply max nums)
-        min-num (apply min nums)]
-    (str/join " " [max-num min-num]))   
-)
-
-
-(println (high-and-low "1 9 3 4 -5"))
+(defn solution [s]
+  ;; (let [pairs (partition 2 2 [\_] s)]
+  ;;   (map #(apply str %) pairs)))
+  (->> s
+      (partition 2 2 [\_])
+      (map #(apply str %))))
+    
+(println (solution "abc"))
